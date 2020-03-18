2b1dd5a862a0e38bc4106716d7c78a53f7a6bcbc
Didik Tri Susanto <rezd14@gmail.com>
2020-03-18 11:02:56 +0700

    bump vendors & delete auth controller

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->route('login');
 });
-Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('/login', function() {
+    return view('auth.login');
+})->name('login');
 Route::post('/auth', function() {
     return redirect()->route('team');
 })->name('dummyauth');
